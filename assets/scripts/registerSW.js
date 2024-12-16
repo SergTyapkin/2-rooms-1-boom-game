f0ab6e27cf0f519e785cf2700c1a6e7379b07f1d
Сергей Тяпкин <Tyapkin2002@mail.ru>
2024-12-16 08:41:47 +0300

    Добавил Service Worker и полное сохранение сайта

--- assets/scripts/registerSW.js
+++ assets/scripts/registerSW.js
@@ -1,38 +1,106 @@
 const SW_FILE_PATH = 'sw.js';
 const SCOPE = '.';
 
-if ('serviceWorker' in navigator) {
+
+// Типы PostMessage для общения приложения с service worker'ом
+const PostMessagesNames = {
+  cacheUrls: 'CACHE_URLS',
+
+  swCacheProgress: 'SW_CACHE_PROGRESS',
+  swAllUrlsCached: 'SW_ALL_URLS_CACHED',
+}
+
+const PostMessage = (type, payload) => ({
+  type: type,
+  payload: payload,
+});
+
+let SW;
+
+if (navigator.serviceWorker) {
   (async () => {
     try {
-      const reg = await navigator.serviceWorker.register(SW_FILE_PATH, { scope: SCOPE });
-      console.log('SW registration success. Scope: ' + reg.scope);
-      const data = {
-        type: 'CACHE_URLS',
-        payload: [
-          location.href,
-          ...performance.getEntriesByType('resource').map(({ name }) => name)
-        ]
-      };
-      const { installing, waiting, active } = reg;
-      if (installing) {
-        console.log('SW installing...');
-      } else if (waiting) {
-        console.log('SW installed');
-      } else if (active) {
-        active.postMessage(data);
-        console.log('Send to ServiceWorker caching urls...');
-      }
+      // if (!navigator.serviceWorker.controller) {
+        const regPromise = navigator.serviceWorker.register(SW_FILE_PATH, { scope: SCOPE });
+        console.log('SW registration...');
+        const reg = await regPromise;
+        console.log('SW registration success. Scopes: ' + reg.scope);
+        reg.onupdatefound = onServiceWorkerUpdated;
+        SW = reg.active || reg.waiting || reg.installing;
+        await onServiceWorkerStateChangedSW(SW);
+      // } else {
+      //   console.log('SW has already been installed');
+      //   await onServiceWorkerStateChangedSW();
+      // }
     } catch(error) {
-      console.log('SW installation failed. Error: ' + error);
+      console.error('SW installation failed. Error: ' + error);
     }
   })();
 }
-navigator.serviceWorker.register(
-  'assets/scripts/sw.js'
-).then(function(registration) {
-  console.log('ServiceWorker registration', registration);
-  // строкой ниже можно прекратить работу serviceWorker’а
-  //registration.unregister();
-}).catch(function(err) {
-  throw new Error('ServiceWorker registration error: ' + err);
-});
+
+function onServiceWorkerUpdated(e) {
+  const sw = e.target.active || e.target.waiting || e.target.installing;
+  sw.onstatechange = onServiceWorkerStateChanged;
+  onServiceWorkerStateChangedSW(sw);
+}
+function onServiceWorkerStateChanged(e = undefined) {
+  onServiceWorkerStateChangedSW(e.target);
+}
+async function onServiceWorkerStateChangedSW(_sw = navigator.serviceWorker.controller) {
+  if (!_sw) {
+    return;
+  }
+  SW = _sw;
+  await _sw.ready;
+
+  console.log('SW state:', SW.state);
+  if (SW.state === 'installed') {
+    if (navigator.serviceWorker.controller) {
+      console.log('SW has already been installed before');
+    } else {
+      console.log('SW new instance installed');
+    }
+  } else if (SW.state === 'activated') {
+    SW.postMessage(PostMessage(
+      PostMessagesNames.cacheUrls,
+      [
+        location.href,
+        ...performance.getEntriesByType('resource').map(({ name }) => name),
+      ]
+    ));
+  }
+}
+
+
+async function serviceWorkerCacheFiles(urls, callbackEach) {
+  if (!SW) {
+    console.error('SW: Error. Can\'t cache urls because SW is not initialized yet');
+    return;
+  }
+  await SW.ready;
+  console.log('Send to ServiceWorker caching urls...');
+  if (SW.state !== 'activated') {
+    console.error('SW: Error. Can\'t cache urls because SW is not activated yet. SW state:', SW.state);
+    return;
+  }
+  let resolvePromise, rejectPromise;
+  const promise = new Promise((resolve, reject) => {resolvePromise = resolve; rejectPromise = reject});
+  navigator.serviceWorker.onmessage = (e) => {
+    console.log('MESSAGE', e)
+    if (e.data.type === PostMessagesNames.swCacheProgress) {
+      callbackEach({
+        current: e.data.payload.current,
+        progress: e.data.payload.progress,
+        total: e.data.payload.total,
+      });
+    } else if (e.data.type === PostMessagesNames.swAllUrlsCached) {
+      resolvePromise();
+    }
+  };
+
+  SW.postMessage(PostMessage(PostMessagesNames.cacheUrls, urls));
+  return promise;
+}
+
+window.serviceWorkerCacheFiles = serviceWorkerCacheFiles;
+export {serviceWorkerCacheFiles};
--- sw.js
+++ sw.js
@@ -1,12 +1,44 @@
-// период обновления кэша - одни сутки
-const MAX_AGE = 86400000;
-
 // наименование для нашего хранилища кэша
 const CACHE_KEYNAME = 'app_serviceworker_v_1';
 
 // regExp-ссылок на НЕ-кэшируемые файлы
 const DISABLE_CACHING_URLS_REGEXPS = [
+  /sw\.js/,
 ];
+
+// для страниц слева будут отдаваться ресурсы справа
+const RESOURCE_MAPPING_REGEXPS = {
+  'role\\.html\\?idx=\\d+': 'role.html',
+};
+
+// Типы PostMessage для общения приложения с service worker'ом
+const PostMessagesNames = {
+  cacheUrls: 'CACHE_URLS',
+
+  swCacheProgress: 'SW_CACHE_PROGRESS',
+  swAllUrlsCached: 'SW_ALL_URLS_CACHED',
+}
+
+
+const PostMessage = (type, payload) => ({
+  type: type,
+  payload: payload,
+});
+
+
+function broadcastPostMessage(message) {
+  // Select who we want to respond to
+  self.clients.matchAll({
+    includeUncontrolled: true,
+    type: 'window',
+  }).then((clients) => {
+    if (clients && clients.length) {
+      // Send a response - the clients
+      // array is ordered by last focused
+      clients[0].postMessage(message);
+    }
+  });
+}
 
 console.log('SW file initialized');
 
@@ -18,20 +50,53 @@ self.addEventListener('active', function(event) {
   console.log('SW active', event);
 });
 
+function isUrlNotCachable(url) {
+  return DISABLE_CACHING_URLS_REGEXPS.some(regExp => regExp.test(url));
+}
+async function downloadAll(urls, callbackEach) {
+  const cache = await caches.open(CACHE_KEYNAME);
+  const promises = [];
+  let finishedCount = 0;
+  urls.forEach(url => {
+    if (isUrlNotCachable(url)) {
+      return;
+    }
+    try {
+      promises.push(cache.add(url).then(() => {
+        finishedCount++;
+        callbackEach({current: url, progress: finishedCount});
+      }));
+    } catch (err) {
+      console.warn('SW: Error on caching file', url, '| Error:', err);
+    }
+  });
+  await Promise.all(promises);
+}
+async function setCached(url, response) {
+  if (isUrlNotCachable(url)) {
+    return;
+  }
+  const cache = await caches.open(CACHE_KEYNAME);
+  await cache.put(url, response.clone());
+}
+
 // Добавление в кэш статики через postMessage, отправленной из основного приложения
 self.addEventListener('message', async (event) => {
-  if (event.data.type === 'CACHE_URLS') {
+  if (event.data.type === PostMessagesNames.cacheUrls) {
     if (!event.data.payload || !Array.isArray(event.data.payload)) {
       console.error('SW: Incorrect PostMessage with caching urls');
       return;
     }
-    try {
-      const cache = await caches.open(CACHE_KEYNAME);
-      await cache.addAll(event.data.payload);
-      console.log('SW: Urls added to cache:', event.data.payload);
-    } catch (error) {
-      console.error('SW: Adding urls to cache error:', error);
-    }
+    await downloadAll(event.data.payload, (data) => {
+      broadcastPostMessage(PostMessage(PostMessagesNames.swCacheProgress, {
+        current: data.current,
+        progress: data.progress,
+        total: event.data.payload.length,
+      }));
+      console.log('SW:', data.progress, data.current);
+    });
+    broadcastPostMessage(PostMessage(PostMessagesNames.swAllUrlsCached, null));
+    console.log('SW: Urls added to cache:', event.data.payload);
   }
 });
 
@@ -40,19 +105,24 @@ self.addEventListener('fetch', function(event) {
 
   event.respondWith(
     (async () => {
-      console.log("FETCH EVENT", event);
       try { // Пытаемся отправить оригинальный запрос
         const response = await fetch(event.request);
         if (response.ok) { // Запрос прошел. Отдаем оригинальный запрос
           // Добавляем в кэш
-          const cache = await caches.open(CACHE_KEYNAME);
-          await cache.put(event.request, response.clone());
+          await setCached(event.request, response);
           // Возвращаем
           return response;
         }
         throw Error('Start respond with cache...'); // Запрос не прошел. Отдаем из кэша
       } catch { // Оригинальный запрос упал. Пытаемся отдать из кэша
-        const cachedResponse = await caches.match(event.request);
+        // Преобразуем url по правилам из конфига в константах
+        let url = event.request.url;
+        const replacingRegExp = Object.keys(RESOURCE_MAPPING_REGEXPS).find(regExpStr => (new RegExp(regExpStr, 'ig')).test(url));
+        if (replacingRegExp) {
+          url = url.replaceAll(new RegExp(replacingRegExp, 'ig'), RESOURCE_MAPPING_REGEXPS[replacingRegExp]);
+        }
+        // Ищем кэш
+        const cachedResponse = await caches.match(url);
         // Отдаем либо кэш, либо ответ 418
         return cachedResponse || new Response(`
 <html lang="en">
@@ -62,7 +132,11 @@ self.addEventListener('fetch', function(event) {
 </head>
 <body align="center"">
   <h1>Нет соединения</h1>
-  <h4>Чтобы згрузить эту страницу, включите соединение</h4>
+  <p>
+    <b>Чтобы згрузить эту страницу, включите соединение</b>
+    <br>
+    <small><i>Или этой страницы не существует</i></small>
+  </p>
 </body>
 </html>`, {headers: { 'Content-Type': 'text/html' }});
       }
